ac8a21a9cc55169e378b7743e7040e72a1e8620a
Adam <adamgensh@gmail.com>
2020-05-16 01:49:40 +0300

    Make an index of all posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,53 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 
 import Page from '../components/Page';
 import Container from '../components/Container';
 import IndexLayout from '../layouts';
 
-const IndexPage = () => (
-  <IndexLayout>
-    <Page>
-      <Container>
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </Container>
-    </Page>
-  </IndexLayout>
-);
+const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+            layout
+          }
+          frontmatter {
+            title
+            layout
+          }
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = () => {
+  const data = useStaticQuery<{ allMarkdownRemark: { edges: { node: { fields: { slug: string }; frontmatter: { title: string } } }[] } }>(
+    query
+  );
+
+  const posts = data.allMarkdownRemark.edges;
+  return (
+    <IndexLayout>
+      <Page>
+        <Container>
+          <h1>Hi people</h1>
+          <p>Here are my latest posts</p>
+
+          <ul>
+            {posts.map(post => (
+              <li key={post.node.fields.slug}>
+                <Link to={post.node.fields.slug}>{post.node.frontmatter.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </Container>
+      </Page>
+    </IndexLayout>
+  );
+};
 
 export default IndexPage;
