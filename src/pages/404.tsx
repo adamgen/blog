9d86fe07765c00b078e43f03424b460ee15adb64
Adam <adamgensh@gmail.com>
2020-05-17 13:19:37 +0300

    Change 404 text

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -11,7 +11,7 @@ const NotFoundPage = () => (
       <Container>
         <h1>404: Page not found.</h1>
         <p>
-          You've hit the void. <Link to="/">Go back.</Link>
+          You've hit the void. <Link to="/">Go home.</Link>
         </p>
       </Container>
     </Page>
